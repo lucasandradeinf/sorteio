9bf0c6284eadac62a182ae368ae993fff2a3663d
lucasandradeinf <lucasandrade.inf@gmail.com>
2015-12-08 16:25:42 -0200

    2

---
+++ sorteio/src/main/java/sorteio/Sessao.java
@@ -0,0 +1,63 @@
+package sorteio;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+@WebServlet(value = "sorteio")
+public class Sessao extends HttpServlet {
+
+	
+	protected void service(
+		      HttpServletRequest req,
+		      HttpServletResponse resp)
+		      throws ServletException, IOException {
+		 ArrayList<Integer> list = new ArrayList<Integer>();
+		 Random r = new Random();
+         String resposta= null;
+         int size = 0;
+       		    //Referência para a sessão.
+		    HttpSession sessao = req.getSession();
+		    
+		    String acao = req.getParameter("acao");
+		    ArrayList l = (ArrayList) sessao.getAttribute("lista");
+		    size = l.size();
+		    
+		    if(acao.equals("Sortear")&&size<10){
+				 int valor = r.nextInt()*10;
+				
+		    	if(sessao.getAttribute("lista") == null){
+		    		list.add(valor);
+			    	sessao.setAttribute("lista", list);
+			    }
+		    	
+		    	if(sessao.getAttribute("lista")!=null){
+		    	for(int i=0;i<list.size();i++){
+		    		if(valor == (Integer)list.get(i)){
+		    			valor = r.nextInt();
+		    		}
+		    	}
+		    	  resposta = Integer.toString(valor);
+		    	  list.add(valor); 
+		    	  sessao.setAttribute("lista", list);
+		    	} 
+			    
+		    }else if(acao.equals("Reiniciar")){
+                          sessao.invalidate();
+		    }else if(size==10){
+		    	resposta = "Já foram inseridos 10 numeros";
+		    }
+		    req.setAttribute("valor", resposta);
+		    
+		    req.getRequestDispatcher("sorteio/sorteio.jsp").forward(req, resp);   
+	 }	
+	
+}
+
---
+++ sorteio/src/main/webapp/sorteio.jsp
@@ -0,0 +1,45 @@
+<%@page import="sessao.SessaoJogo"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<div class="container">
+
+<div class="jumbotron">
+    <h1>Sorteio de 1 a 10</h1> 
+    <p>Click em sortear
+         </p> 
+  </div>
+  <br>
+  <br>
+  <br>
+   </h1> <% String t = (String) request.getAttribute("valor"); 
+    if(t == null){%><h1>
+    <div class="alert alert-info">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong>Click no botão Sortear!</strong> 
+    </div>
+    <%}else{%>
+    <div class="alert alert-info">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong><%out.println(t);%></strong> 
+    </div>
+    <%}%>
+  
+  <button type="button" class="btn btn-success"  name="acao">Sortear</button>
+  <button type="button" class="btn btn-danger"  name="acao">Reiniciar</button>
+</div>  
+
+</body>
+</html>
